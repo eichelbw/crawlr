e838f6c711bad6a5f3f67a1c44c29d81d9ede322
W Eich <rw.eichelberger@gmail.com>
2014-12-12 14:50:02 -0800

    yet another rewrite of the twitter scraper. in progress. def shoulda checked out a different branch.

--- t_gttr_v2.py
+++ t_gttr_v2.py
@@ -1,12 +1,18 @@
 import tweepy
 from tweepy.streaming import StreamListener
+from datetime import datetime
+import os
 import json
-from bs4 import BeautifulSoup
 import config
 
 consumer_key, consumer_secret = config.CONSUMER_KEY, config.CONSUMER_SECRET
 access_token = config.ACCESS_TOKEN
 access_token_secret = config.ACCESS_TOKEN_SECRET
+
+# much of this structure is heavily inspire by 
+# https://github.com/computermacgyver/twitter-python/blob/master/streaming.py
+# that version saves json objects, and this one grabs text and coerces
+# it into a roughly csv format.
 
 class scraper:
 
@@ -25,16 +31,21 @@ class listener(StreamListener):
     """pretty basic streaming api listener."""
 
     def __init__(self, target):
-        StreamListener.__init__(self)
-        self.target = target
+        super(StreamListener, self).__init__(self)
+        self.base_path = target
+        os.system("touch %s" % target)
 
-    def on_data(self, data):
-        jsn = json.loads(data)
-        if jsn['lang'] == 'en': # only english words for now
-            if jsn['text'][:2] == "RT": # don't care who's being retweeted
-                tweet(jsn['text'].split(": ")[1], self.target).commit()
-            else:
-                tweet(jsn['text'], self.target).commit()
+    d = datetime.today()
+    self.filename = "%i-%02d-%02d.csv" % (d.year, d.month, d.day)
+    self.fh = open(self.base_path + "/" + self.filename, "a")
+
+    self.tweet_count = 0
+    self.error_count = 0
+    self.limit_count = 0
+    self.last = datetime.now()
+
+    def on_data(self, data): # called when the listener receivs a new tweet
+        tweet(json.loads(data), self.base_path).commit()
         return True
 
     def on_error(self, status):
@@ -46,17 +57,22 @@ class listener(StreamListener):
 class tweet:
     """takes info from listener, formats it, commits it to csv"""
 
-    def __init__(self, in_text, target):
-        print "tweet init"
-        self.in_text = in_text
+    def __init__(self, jsn, target):
+        self.jsn = jsn
         self.target = target
-        self.tweet_text = self.csv_format(self.in_text)
+        self.tweet_text = self.csv_format(self.jsn)
 
-    def csv_format(self, in_txt):
-        in_txt = in_txt.split()
-        in_txt = [word.lower().strip('\'\"-,.:;!?') for word in in_txt]
+    def csv_format(self, in_jsn):
+        if in_jsn['lang'] == 'en': # only english words for now
+            if in_jsn['text'][:2] == 'RT': # don't care who's being retweeted
+                in_jsn = in_jsn['text'].split(": ")[1].split()
+            else:
+                in_jsn = in_jsn['text'].split()
+
+        in_jsn = [word.lower().strip('\'\"-,.:;!?') for word in in_jsn]
         out_txt = []
-        for word in in_txt:
+
+        for word in in_jsn:
             try:
                 if word == "&amp": # discard a bunch of nonsense
                     pass
@@ -78,4 +94,11 @@ class tweet:
             f.write(self.tweet_text + "\n")
 
 
+if __name__ == '__main__':
+    while True:
+        try:
+            # create the listener
+            listener = listener()
+        except TimeoutException:
+            print "%s - Timeout exception caught. Closing stream." % datetime.now()
 
